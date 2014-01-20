f6f4731dc3e918d71ba7dd41427be5a2070f90f2
Andrew Nestorenko <andrew.nestorenko@gmail.com>
2014-01-20 22:10:43 +0200

    grunt-watcher is added watch task is in progress

--- gruntfile.js
+++ gruntfile.js
@@ -5,69 +5,65 @@
  * Copyright (c) 2013 Andy Potanin
  * Licensed under the MIT license.
  */
-
 'use strict';
-
 module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+//        jshint: {
+//            all: [
+//                'Gruntfile.js', 'tasks/*.js', '<%= nodeunit.tests %>'
+//            ],
+//            options: {
+//                jshintrc: '.jshintrc'
+//            }
+//        },
+//        // Before generating any new files, remove any previously-created files.
+//        clean: {
+//            tests: ['tmp']
+//        },
+        // Configuration to be run (and then tested).
+        google_cloud_storage: {
+            default_options: {
+                options: {
+                },
+                files: {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
-
-    // Configuration to be run (and then tested).
-    google_cloud_storage: {
-      default_options: {
-        options: {
+                }
+            }
         },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+        watch : {
+            all: {
+                files: ['cdn/*'],
+                tasks: ['google_cloud_storage'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+//        // Unit tests.
+//        nodeunit: {
+//            tests: ['test/*_test.js']
+//        }
 
-  });
+    });
+    grunt.event.on('watch', function(action, filepath) {
+        console.log('watch', action, filepath);
+        grunt.config('google_cloud_storage.default_options.files', filepath);
+        grunt.config('google_cloud_storage.default_options.event', action);
+    });
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'google_cloud_storage', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
-
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'google_cloud_storage', 'nodeunit']);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['watch']);
 };
